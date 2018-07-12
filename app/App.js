531502f01a7cf5e9ae2c7bb44467c695f7fac4b0
Nichika Yoshida <n_yoshida@sackle.co.jp>
2018-07-12 16:40:52 +0900

    create newfeed.js

--- app/App.js
+++ app/App.js
@@ -14,88 +14,10 @@ import LoadingIndicator from './components/LoadingIndicator'
 import ArticleProvider from './dataProvider/ArticleProvider'
 import { CenteredTextModal } from './components/Modal'
 import { getStatusBarHeight } from 'react-native-status-bar-height'
+import NewFeed from './components/NewFeed'
 
 export default class App extends Component {
-  state = {
-    threads: [],
-    isModalVisible: true,
-    refreshing: false
-  }
-
-  componentDidMount() {
-    this._getThreads()
-  }
-
-  _getThreads = async () => {
-    try {
-      let threads = await ArticleProvider.getThreads()
-      this.setState({ threads: threads, refreshing: false })
-    } catch (e) {
-      console.error(e)
-    }
-  }
-
-  _refreshList() {
-    this.setState({ refreshing: true })
-    this._getThreads()
-  }
-
-  _articleFlatList = () => (
-    <FlatList
-      data={this.state.threads}
-      ItemSeparatorComponent={Separator}
-      refreshControl={
-        <RefreshControl
-          refreshing={this.state.refreshing}
-          onRefresh={this._refreshList.bind(this)}
-        />
-      }
-      renderItem={({ item, index }) => {
-        const bgColor = index % 2 == 0 ? 'white' : 'lightgray'
-        const { width } = Dimensions.get('window')
-
-        return <Article bgColor={bgColor} width={width} item={item} />
-      }}
-    />
-  )
-
-  _onBackdropPress() {
-    this.setState({ isModalVisible: false })
-  }
-
   render() {
-    return this.state.threads.length == 0 ? (
-      <View style={styles.container}>
-        <View style={styles.statusBar} />
-        <View style={styles.container}>
-          <LoadingIndicator />
-        </View>
-      </View>
-    ) : (
-      <View style={styles.container}>
-        <View style={styles.statusBar} />
-        {this._articleFlatList()}
-        <CenteredTextModal
-          isVisible={this.state.isModalVisible}
-          onBackdropPress={() => this.setState({ isModalVisible: false })}
-          text="Welcome!! Enjoy React Native!!"
-        />
-      </View>
-    )
+    return <NewFeed />
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white'
-  },
-  statusBar: {
-    height: getStatusBarHeight(),
-    width: '100%',
-    backgroundColor: 'black'
-  }
-})
